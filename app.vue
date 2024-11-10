d344cc601a9b48fb9c9d24b130ba4b9327ce002a
iamsamienda <samuelendale7373@gmail.com>
2024-11-10 10:12:52 +0300

    add layout components for course overview in app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen bg-white">
+    
+    <CourseHeader />
+    <main>
+      <CourseHero />
+      <CourseCertificate />
+      <WhatYouLearn />
+      <CourseTopics />
+      <CourseIncludes />
+      <CourseContent />
+
+      
+    </main>
   </div>
 </template>
