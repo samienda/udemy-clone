c1cd8b16933a0e08d8859b45146893f2b2bc00ba
iamsamienda <samuelendale7373@gmail.com>
2024-11-10 09:16:05 +0300

    add CourseCertificate component to display certification details

---
+++ components/CourseCertificate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-white lg:mx-10 p-6 m-2 rounded-lg shadow-sm border border-gray-200 lg:w-3/5">
+      <p class="text-gray-800 mb-2">
+        Prepare for your certification with this course. 
+        <ULink href="#" class="text-primary-600">Learn more</ULink>
+      </p>
+      
+      <div class="flex items-center gap-4">
+        <img 
+          src="../public/aws-badge.png" 
+          alt="AWS Certification Badge" 
+          class="w-16 h-16"
+        />
+        <div>
+          <h3 class="text-primary-600 font-medium">
+            AWS Certified Solutions Architect – Associate
+          </h3>
+          <p class="text-sm text-gray-600">
+            Issued by Amazon Web Services Training and Certification
+          </p>
+        </div>
+      </div>
+    </div>
+  </template>
