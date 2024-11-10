dc8e00cf5024f9a7bde135064016e526ce71c818
iamsamienda <samuelendale7373@gmail.com>
2024-11-10 09:15:40 +0300

    add CourseBreadcrumb component with predefined navigation links

---
+++ components/CourseBreadcrumb.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="bg-gray-100 lg:bg-gray-900 pb-3">
+    <div class="container mx-auto px-4">
+      <UBreadcrumb :links="links" />
+    </div>
+  </nav>
+</template>
+
+<script setup>
+const links = [
+  { label: 'IT & Software', to: '#' },
+  { label: 'IT Certifications', to: '#' },
+  { label: 'AWS Certified Solutions Architect - Associate', to: '#' }
+]
+</script>
