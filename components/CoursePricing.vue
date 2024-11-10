f6dab659c6ba154b1cb2780d6ab126c628aed872
iamsamienda <samuelendale7373@gmail.com>
2024-11-10 09:18:16 +0300

    add CoursePricing component to display pricing details with a desktop layout

---
+++ components/CoursePricing.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Desktop Pricing Card -->
+  <div class="hidden lg:block pr-16">
+    <div class="bg-white rounded-lg shadow-xl transition-opacity duration-300">
+      <!-- Video Preview -->
+      <div class="relative">
+        <img 
+          src="../public/aws-cert-preview.png" 
+          alt="Course Preview"
+          class="w-full h-64 object-cover rounded-t-lg p-0.5"
+        />
+        
+      </div>
+
+      <!-- <PricingTabs /> -->
+   
+
+ 
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useScroll } from '@vueuse/core'
+
+const { y } = useScroll(window)
+
+// Add scroll-based opacity transition if needed
+</script>
